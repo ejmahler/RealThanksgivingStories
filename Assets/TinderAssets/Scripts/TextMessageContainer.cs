eb0464fc9138657e98afc3c932e0be8a915e5fdd
Elliott Mahler <join.together@gmail.com>
2018-11-18 14:47:57 -0800

    Added scaffold for tinder screen 2

---
+++ Assets/TinderAssets/Scripts/TextMessageContainer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextMessageContainer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddTextMessage(RectTransform MessagePrefab, string MessageText) {
+
+    }
+}
