934bb22447d188cc8d48f35b1aa0aadad4fcae58
Elliott Mahler <join.together@gmail.com>
2018-11-18 21:23:44 -0800

    Tweaks to intro song and noise

--- Assets/WitchTrialAssets/BumBumManager.cs
+++ Assets/WitchTrialAssets/BumBumManager.cs
@@ -10,7 +10,7 @@ public class BumBumManager : MonoBehaviour {
 
 	// Use this for initialization
     IEnumerator Start () {
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(2.0f);
         GetComponent<FadeToBlackScript>().currentStatus = FadeToBlackScript.FadeStatus.FadingToTransparent;
         DontDestroyOnLoad(gameObject);
         SceneManager.LoadScene(_NextScene);
